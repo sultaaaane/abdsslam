b20b1a79d24d6fff180ee145df66b0c6ff5ba87c
sultaaaane <mohammed_bentahiralaoui@um5.ac.ma>
2023-04-17 15:36:02 +0000

    test

--- php/DB/connect.php
+++ php/DB/connect.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'tax_expert');
+define('DB_NAME', 'tax-expert');
 
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
