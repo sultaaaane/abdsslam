a9a334af5e5ff22473e5aa4b34ed374dd0e9486d
sultaaaane <mohammed_bentahiralaoui@um5.ac.ma>
2023-04-21 02:03:34 +0000

    dd

--- php/admin/client.php
+++ php/admin/client.php
@@ -4,18 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mes clients</title>
-</head>
-<body>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gerer le types</title>
     <link rel="stylesheet" href="css/gererType.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style/sidebar.css">
 </head>
 <body>
     
