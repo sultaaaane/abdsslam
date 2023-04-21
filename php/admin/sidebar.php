9101c288cd5db6afe2beef612f4cdf5924cdf834
sultaaaane <mohammed_bentahiralaoui@um5.ac.ma>
2023-04-21 01:47:24 +0000

    g

--- php/admin/sidebar.php
+++ php/admin/sidebar.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="style/sidebar.css">
+    <link rel="stylesheet" href="../style/sidebar.css">
     <!-- Boxicons CDN Link -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
