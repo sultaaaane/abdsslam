9778568e67877a9da575c4e7da7c102c21bc67bc
Nouamanex <chagourinouamane@gmail.com>
2023-04-27 14:22:49 +0100

    ertyu

--- php/Client/index.php
+++ php/Client/index.php
@@ -9,7 +9,7 @@
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
-     <link rel="stylesheet" href="../../vendor/cdnjs/sweetalert.min.js">
+     <script src="../../vendor/cdnjs/sweetalert.min.js"></script>
 <title></title>
 <style>
   #client{
